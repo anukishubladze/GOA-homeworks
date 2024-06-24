51754c83b2467f4df1ece0e10afb259af1396d12
anukishubladze <anastasiagold2007@gmail.com>
2024-06-24 13:33:28 +0400

    first commit

---
+++ level 01/homework/lesson1.py
@@ -0,0 +1,11 @@
+name = "anuki"
+#name aris cvladi
+# = aris cvladze mnisvnelobis mimniwebeli simbolo
+# "anuki" aris cvladistvis mnisvneloba
+surname = "shubladze"
+
+print(name)
+#print funqcias gadaecema ekranze gamosatani cvladi an stringi (obieqti)
+
+
+print("me var anuki shubladze. var 16 wlis. simagleshi var 178. vcxovrob abashashi.")
---
+++ level 02/homework/lesson1.py
@@ -0,0 +1,1 @@
+me gavicani nino xvichia

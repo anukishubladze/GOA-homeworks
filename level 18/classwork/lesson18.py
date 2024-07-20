e759e2e7fcfcf6e3141e6584d548075a5f50cc8d
anukishubladze <anastasiagold2007@gmail.com>
2024-07-20 22:46:18 +0400

    first commit

---
+++ level 18/classwork/lesson18.py
@@ -0,0 +1,23 @@
+cars = {
+    "name": "dodge",
+    "mark": "dodge hellcat",
+    "year": 2015,
+    "engine": " 5.7-and 6.4-liter HEMI V8 series, it is engineered from the block up to deliver more than 700-horsepower of turbocharged power",
+}
+arry = ["dodge", 2015]
+print(cars)
+
+person = {
+    "name": "anuki",
+    "surname": "shubladze",
+    "age": 16,
+}
+
+
+cars.update({"email" , "username"})
+print("email")
+print("surname")
+
+
+
+
---
+++ level 18/homework/lesson18.py
@@ -0,0 +1,10 @@
+person={
+    "name": "anuki",
+    "age" : 16,
+    "city": "abasha",
+    "work": "programmer"
+}
+print(person)
+
+
+

b0e055b9147609d65cb9883858ae382637f8b8e7
anukishubladze <anastasiagold2007@gmail.com>
2024-08-13 17:27:05 +0400

    first commit

---
+++ level 27/classwork/lessson27.py index.c
@@ -0,0 +1,21 @@
+#div1, #div2, #div3{
+            width: 30%;
+            height: 400px;
+            background-color: green;
+        }
+        #div1-div{
+            width: 70%;
+            height: 20%;
+            background-color: rgb(117, 107, 107);
+        }
+        #div2-div{
+            width: 50%;
+            height: 50%;
+            background-color: rgb(122, 82, 82);
+        }
+        #div3-div{
+            width: 10%;
+            height: 80%;
+            background-color: rgb(41, 4, 4);
+            color: white;
+        }
